ff0a830e9e75577326a1d1284a4ade7746f5ffdb
Alejandro Gallo <aamsgallo@gmail.com>
2021-02-13 17:02:24 +0100

    Add use-package to site.el

--- site.el
+++ site.el
@@ -1,6 +1,45 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(setq package-archives
+  '(("gnu"   . "http://elpa.gnu.org/packages/")
+    ("melpa" . "http://melpa.org/packages/"   )
+    ("org"   . "http://orgmode.org/elpa/"     )))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-when-compile
+  (require 'use-package))
+
+;(add-to-list 'load-path "./lisp/external/htmlize")
+;(require 'htmlize)
+
+(use-package htmlize
+             :ensure t)
+
+(use-package raku-mode
+             :ensure t)
+
 (require 'org)
-(add-to-list 'load-path "./lisp/external/htmlize")
-(require 'htmlize)
+
+(use-package org-plus-contrib
+             :ensure t)
+
+(use-package org-src
+             :ensure t)
+
+(use-package org-src
+  :config
+  (setq org-src-fontify-natively t
+        org-src-preserve-indentation t
+        org-src-tab-acts-natively t))
+
+(load-theme 'tsdh-light)
+
+
 
 (defun publish-site ()
   (interactive)
@@ -18,6 +57,4 @@
              :exclude ".*templates.*"
              :recursive t))))
       (message "\x1b[35m∀ Publishing site...\x1b[0m")
-      (org-publish-current-project)
-    )
-  )
+      (org-publish-current-project)))
