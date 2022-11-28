95e5276c702747e67863552e5beead0fe34d2f65
Alejandro Gallo <aamsgallo@gmail.com>
2022-11-28 01:53:52 +0100

    Update config.el

--- blog/content-org/config.el
+++ blog/content-org/config.el
@@ -4,7 +4,7 @@
   (interactive)
   (unless (buffer-narrowed-p)
     (error "You should be in a narrowed place to do this"))
-  (org-hugo-export-to-md))
+  (org-hugo-export-wim-to-md))
 
 
 (defun blog-new (fname)
